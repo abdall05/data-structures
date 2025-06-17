4fe6cfa3ee16adda14805233e7dd093b188ab90e
Ali Abdallah <abdall05ali05@gmail.com>
2025-06-17 09:25:52 +0200

    finish circular array

--- Array/circularArray.cpp
+++ Array/circularArray.cpp
@@ -14,11 +14,13 @@ private:
 
 public:
     CircularArray(size_t size = DEFAULT_SIZE);
+    ~CircularArray();
     bool isEmpty();
     bool isFull();
-    void enqueue(T val);
-    void dequeue();
-    T peek()
+    void addRear(T val);
+    T removeFront();
+    T peek();
+    void print();
 };
 
 template <typename T>
@@ -26,9 +28,14 @@ CircularArray<T>::CircularArray(size_t size)
 {
     rear = front = -1;
     capacity = size;
-    data = new T[capacity + 1];
+    data = new T[capacity];
 }
 
+template <typename T>
+CircularArray<T>::~CircularArray()
+{
+    delete[] data;
+}
 template <typename T>
 bool CircularArray<T>::isEmpty()
 {
@@ -38,5 +45,82 @@ bool CircularArray<T>::isEmpty()
 template <typename T>
 bool CircularArray<T>::isFull()
 {
-    return rear % capacity == front;
+    return (rear + 1) % capacity == front;
+}
+
+template <typename T>
+void CircularArray<T>::addRear(T val)
+{
+    if (isFull())
+        resize();
+    if (isEmpty())
+    {
+        rear = front = 0;
+        data[rear] = val;
+    }
+    else
+    {
+        rear = (rear + 1) % capacity;
+        data[rear] = val;
+    }
+}
+
+template <typename T>
+T CircularArray<T>::removeFront()
+{
+    if (isEmpty())
+        throw std::runtime_error("Array empty");
+    T val = data[front];
+    if (rear == front)
+    {
+        front = rear = -1;
+    }
+    else
+    {
+        front = (front + 1) % capacity;
+    }
+    return val;
+}
+template <typename T>
+T CircularArray<T>::peek()
+{
+    if (isEmpty())
+        throw std::runtime_error("Array empty");
+    return data[front];
+}
+template <typename T>
+void CircularArray<T>::resize()
+{
+    T *newData = new T[2 * capacity];
+    for (int i = 0; i < capacity; i++)
+    {
+        newData[i] = data[(front + i) % capacity];
+    }
+    delete[] data;
+    data = newData;
+    front = 0;
+    rear = capacity - 1;
+    capacity *= 2;
+}
+
+template <typename T>
+void CircularArray<T>::print()
+{
+    std::cout << "Front:" << front << "| Rear:" << rear << '\n';
+    for (int i = 0; i < capacity; i++)
+    {
+        std::cout << data[i] << ' ';
+    }
+    std::cout << '\n';
+}
+
+int main()
+{
+    CircularArray<int> arr;
+    for (int i = 0; i < 8; i++)
+        arr.addRear(-1);
+    arr.print();
+    for (int i = 0; i < 3; i++)
+        arr.removeFront();
+    arr.print();
 }
