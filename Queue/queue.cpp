a65110ed24cc46ef2e55580be7953dd71522955e
Ali Abdallah <abdall05ali05@gmail.com>
2025-06-17 09:50:10 +0200

    add Queue implementation

--- Queue/queue.cpp
+++ Queue/queue.cpp
@@ -1,92 +1,141 @@
 #include <iostream>
 
-using namespace std ; 
+// using circular array
 
-//using circular array 
-class queue
+const int DEFAULT_SIZE = 8;
+
+template <typename T>
+class CircularArray
 {
-private: 
-	int *arr ;  
-	int capacity; 
-	int front ; 
-	int rear; 
+private:
+	T *data;
+	size_t capacity;
+	int rear;
+	int front;
+	void resize();
+
 public:
-	queue(int size)
+	CircularArray(size_t size = DEFAULT_SIZE);
+	~CircularArray();
+	bool isEmpty();
+	bool isFull();
+	void addRear(T val);
+	T removeFront();
+	T peek();
+};
+
+template <typename T>
+CircularArray<T>::CircularArray(size_t size)
+{
+	rear = front = -1;
+	capacity = size;
+	data = new T[capacity];
+}
+
+template <typename T>
+CircularArray<T>::~CircularArray()
+{
+	delete[] data;
+}
+template <typename T>
+bool CircularArray<T>::isEmpty()
+{
+	return rear == -1 && front == -1;
+}
+
+template <typename T>
+bool CircularArray<T>::isFull()
+{
+	return (rear + 1) % capacity == front;
+}
+
+template <typename T>
+void CircularArray<T>::addRear(T val)
+{
+	if (isFull())
+		resize();
+	if (isEmpty())
 	{
-		capacity=size; 
-		arr=new int[capacity];
-		front=rear=-1; 
+		rear = front = 0;
+		data[rear] = val;
+	}
+	else
+	{
+		rear = (rear + 1) % capacity;
+		data[rear] = val;
+	}
+}
+
+template <typename T>
+T CircularArray<T>::removeFront()
+{
+	if (isEmpty())
+		throw std::runtime_error("Array empty");
+	T val = data[front];
+	if (rear == front)
+	{
+		front = rear = -1;
+	}
+	else
+	{
+		front = (front + 1) % capacity;
+	}
+	return val;
+}
+template <typename T>
+T CircularArray<T>::peek()
+{
+	if (isEmpty())
+		throw std::runtime_error("Array empty");
+	return data[front];
+}
+template <typename T>
+void CircularArray<T>::resize()
+{
+	T *newData = new T[2 * capacity];
+	for (int i = 0; i < capacity; i++)
+	{
+		newData[i] = data[(front + i) % capacity];
+	}
+	delete[] data;
+	data = newData;
+	front = 0;
+	rear = capacity - 1;
+	capacity *= 2;
+}
+
+// Queue implementation
+template <typename T>
+class Queue
+{
+private:
+	CircularArray<T> arr;
+
+public:
+	Queue(size_t size = DEFAULT_SIZE) : arr(size)
+	{
 	}
 	bool isEmpty()
 	{
-		return front ==-1 && rear==-1 ; 
+		return arr.isEmpty();
 	}
 	bool isFull()
 	{
-		return (rear+1)%capacity==front ; 
+		return arr.isFull();
 	}
 
-	void enqueue(int data )
+	void enqueue(T val)
 	{
-		if(isEmpty())
-		{
-			front=0; 
-			rear=0;
-		}
-		else if (isFull())
-		{
-			return ; 
-		}
-		else 
-		{
-			rear=(rear+1)%capacity;
-		}
-		arr[rear]=data; 
+		arr.addRear(val);
 	}
 
-	void pop()
+	void dequeue()
 	{
-		if(isEmpty())
-			return ;
-		else if (front==rear) // empty after deletion 
-		{
-			rear=-1 ; 
-			front=-1; 
-		}
-
-		else 
-			rear=(rear-1)%capacity;
-		 
-
-
+		arr.removeFront();
 	}
 
-	int getFront()
+	T getFront()
 	{
-		return arr[front];
-
+		return arr.peek();
 	}
-
-	int getRear()
-	{
-		return arr[rear];
-	}
-
-	
-	
 };
-
-int main()
-{
-	queue q(3);
-	for(int i=1;i<=3;i++)
-	{
-		q.enqueue(i);
-	}
-	cout<<q.isFull(); 
-	for(int i=0;i<3;i++)
-		q.pop();
-	cout<<q.isEmpty();
-
-	return 0 ; 
-}
